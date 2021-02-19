820a404ac9dc91b13703003693685bbb9722528f
Jassar alakhras <jalakhras@outlook.com>
2021-02-20 01:20:58 +0200

    config swagger

--- Library.API/Startup.cs
+++ Library.API/Startup.cs
@@ -76,6 +76,15 @@ namespace Library.API
             services.AddScoped<IAuthorRepository, AuthorRepository>();
 
             services.AddAutoMapper();
+            services.AddSwaggerGen(setupAction =>
+            {
+                setupAction.SwaggerDoc("LibraryOpenAPISpecification",
+                    new Microsoft.OpenApi.Models.OpenApiInfo()
+                    {
+                        Title = "Library API",
+                        Version = "1"
+                    });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -93,7 +102,14 @@ namespace Library.API
             }
 
             app.UseHttpsRedirection();
-
+            app.UseSwagger();
+            app.UseSwaggerUI(setupAction =>
+            {
+                setupAction.SwaggerEndpoint(
+                    "/swagger/LibraryOpenAPISpecification/swagger.json",
+                    "Library API");
+                setupAction.RoutePrefix = "";
+            });
             app.UseStaticFiles();
 
             app.UseMvc();
