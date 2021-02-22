049dcf0e031aaa1266a320236e0dadaeda2366b5
Jassar alakhras <jalakhras@outlook.com>
2021-02-22 02:26:46 +0200

    Apply Custom Conventions

--- Library.API/Controllers/ConventionTestsController.cs
+++ Library.API/Controllers/ConventionTestsController.cs
@@ -11,6 +11,7 @@ namespace Library.API.Controllers
     [Route("api/[controller]")]
     [ApiController]
     //[ApiConventionType(typeof(DefaultApiConventions))]
+    [ApiConventionType(typeof(CustomConventions))]
     public class ConventionTestsController : ControllerBase
     {
         // GET: api/<ConventionTestsController>
@@ -30,7 +31,8 @@ namespace Library.API.Controllers
 
         // POST api/<ConventionTestsController>
         [HttpPost]
-        public void Post([FromBody] string value)
+        [ApiConventionMethod(typeof(CustomConventions),nameof(CustomConventions.Insert))]
+        public void Insert([FromBody] string value)
         {
         }
 
